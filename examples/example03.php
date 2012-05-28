86389206bf3635baf8ccb9a63f967beb6db16d9b
Joan-Alexander Grigorov <sasquatch.zaclan@gmail.com>
2012-05-29 01:39:15 +0300

    New example added

---
+++ examples/example03.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once '../lib/Elegance/Db.php';
+
+$db = new Elegance_Db(new PDO('mysql:host=localhost;dbname=test', 'root', ''));
+
+$name = $db->fetchOne('SELECT author FROM `my_fav_albums` WHERE `id` = ?', array(4));
+echo 'My favorite thrash band is ' . $name;
