593a0d6664d236cc6ae8ddb0fd300c5f58a9c027
Joan-Alexander Grigorov <sasquatch.zaclan@gmail.com>
2012-05-29 01:19:00 +0300

    Added two new examples

--- examples/example02.php
+++ examples/example02.php
@@ -5,4 +5,7 @@ require_once '../lib/Elegance/Db.php';
 $db = new Elegance_Db(new PDO('mysql:host=localhost;dbname=test', 'root', ''));
 
 $rows = $db->fetchAll('SELECT * FROM my_fav_albums WHERE `year` < ?', array(1990));
-var_dump($rows);
+
+foreach ($rows as $row) {
+    echo 'Album: ' . $row->name . '; Year: ' . $row->year . '; By: ' . $row->author . PHP_EOL;
+}
