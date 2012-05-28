988eb558bcc29d1dcb23e4979180b6f5970a81bb
Joan-Alexander Grigorov <sasquatch.zaclan@gmail.com>
2012-05-29 02:02:35 +0300

    New examples added

---
+++ examples/example04.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once '../lib/Elegance/Db.php';
+
+$db = new Elegance_Db(new PDO('mysql:host=localhost;dbname=test', 'root', ''));
+
+$pairs = $db->fetchPair('SELECT `id`, `name` FROM `my_fav_albums`');
+var_dump($pairs);
---
+++ examples/example06.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once '../lib/Elegance/Db.php';
+
+$db = new Elegance_Db(new PDO('mysql:host=localhost;dbname=test', 'root', ''));
+
+$data = array(
+        'name' => 'All we got is uz',
+        'author' => 'Onyx',
+        'year' => 1995
+);
+$id = $db->insert('records', $data);
